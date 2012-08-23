a63b32c7a7673ccfd24593ed69e57e43edfefb1b
D.B. Tsai <dbtsai@dbtsai.com>
2012-08-22 21:39:55 -0700

    Uploaded to PyPI using python-mimeparse as package name.

--- setup.py
+++ setup.py
@@ -1,41 +1,34 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
+import os
+import mimeparse
 from setuptools import setup
 
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
-    name='mimeparse',
-    version='0.1.3',
-    description='A module provides basic functions for parsing mime-type names and matching them against a list of media-ranges.',
-    long_description="""
-This module provides basic functions for handling mime-types. It can handle
-matching mime-types against a list of media-ranges. See section 14.1 of
-the HTTP specification [RFC 2616] for a complete explanation.
-
-   http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1
-
-Contents:
-    - parse_mime_type():   Parses a mime-type into its component parts.
-    - parse_media_range(): Media-ranges are mime-types with wild-cards and a 'q' quality parameter.
-    - quality():           Determines the quality ('q') of a mime-type when compared against a list of media-ranges.
-    - quality_parsed():    Just like quality() except the second parameter must be pre-parsed.
-    - best_match():        Choose the mime-type with the highest quality ('q') from a list of candidates.
-      """,
+    name="python-mimeparse",
+    py_modules=["mimeparse"],
+    version=mimeparse.__version__,
+    description=("A module provides basic functions for parsing mime-type "
+                 "names and matching them against a list of media-ranges."),
+    author="David Tsai",
+    author_email="dbtsai@dbtsai.com",
+    url="https://github.com/dbtsai/python-mimeparse",
+    download_url=("http://pypi.python.org/packages/source/p/python-mimeparse/"
+                  "python-mimeparse-" + mimeparse.__version__ + ".tar.gz"),
+    keywords=["mime-type"],
     classifiers=[
-        # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    keywords='mime-type',
-    author='Joe Gregorio',
-    author_email='joe@bitworking.org',
-    maintainer='Joe Gregorio',
-    maintainer_email='joe@bitworking.org',
-    url='http://code.google.com/p/mimeparse/',
-    license='MIT',
-    py_modules=['mimeparse'],
-    zip_safe=True,
+    long_description=read('README')
 )
