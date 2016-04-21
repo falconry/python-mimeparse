cbf1c8686fdef2b1459b714d49856338f63c574e
Ville Skyttä <ville.skytta@iki.fi>
2016-04-21 07:17:42 +0300

    Spelling fix (#15)

--- mimeparse.py
+++ mimeparse.py
@@ -120,7 +120,7 @@ def quality_parsed(mime_type, parsed_ranges):
     Find the best match for a given mime-type against a list of media_ranges
     that have already been parsed by parse_media_range(). Returns the 'q'
     quality parameter of the best match, 0 if no match was found. This function
-    bahaves the same as quality() except that 'parsed_ranges' must be a list of
+    behaves the same as quality() except that 'parsed_ranges' must be a list of
     parsed media ranges. """
 
     return quality_and_fitness_parsed(mime_type, parsed_ranges)[0]
